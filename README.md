e6f60e2e12b7ed7260e534444b900aa62832389e
Neale Philippe <neale@adflexmarketing.com.au>
2022-05-25 12:43:52 +1000

    Updated category.js and readMe.MD

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# -ORM-E-Commerce-Back-End-Week-13
+
+<h1 align="center">(ORM): E-Commerce Back End 👋</h1>
+  
+## Table of Contents
+- [Tasks](#Tasks)
+- [UserStory](#UserStory)
+- [installations](#installations)
+- [Contributers](#contributers)
+
+## Tasks
+Internet retail, also known as **e-commerce**, is the largest sector of the electronics industry, generating an estimated $29 trillion in 2019. E-commerce platforms like Shopify and WooCommerce provide a suite of services to businesses of all sizes. Due to their prevalence, understanding the fundamental architecture of these platforms will benefit you as a full-stack web developer.Your task is to build the back end for an e-commerce site by modifying starter code. You’ll configure a working Express.js API to use Sequelize to interact with a MySQL database.
+
+## User Story
+ As a manager of internet retail store want the back end for the ecommerce website that uses the latest technologies so that the company can compete with other e-commerce   companies
+
+## installations
+ Express.js, MYSQL2,Sequelize, dotenv, inqurier
+
+## Contributers
+Neale
+
+Find me on GitHub: [Neale2021](https://github.com/Neale2021)<br />
+<br />
+✉️ Email me : neale@adflexmarketing.com.au<br /><br />
+    
---
+++ Utilise/index.js
@@ -0,0 +1,64 @@
+
+//dependancies
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./Readme generatorfile.js")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Title",
+            message: "What is the title of your project?",
+        },
+        {
+            type: "input",
+            name: "Tasks",
+            message: "explain your tasks on of your project: "
+        },
+        {
+            type: "input",
+            name: "userStory",
+            message: "What is the users storys?"
+        },
+        {
+            type: "input",
+            name: "installations",
+            message: "What have you installed to create your project? ",
+        },
+
+        {
+            type: "input",
+            name: "Contributers",
+            message: "Who contributed to complete your project?"
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
+
+  async function init() {
+    try {
+        // User questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Create and write new README.md to file
+        await writeFileAsync('/Users/nealephilippe/Desktop/Week 13 chellenge/-ORM-E-Commerce-Back-End-Week-13/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
